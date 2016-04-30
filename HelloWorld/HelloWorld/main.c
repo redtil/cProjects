7118a390bffb95af3d280a7bb9f7fa147f318735
Rediet Tilahun Desta <redtil10@gmail.com>
2016-04-30 13:45:41 +0300

    first commit

---
+++ HelloWorld/HelloWorld/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  HelloWorld
+//
+//  Created by Rediet Tilahun Desta on 4/30/16.
+//  Copyright © 2016 Rediet Tilahun Desta. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, Wor!\n");
+    return 0;
+}
